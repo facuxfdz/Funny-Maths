7c2891235316dcafc5baf41ee1c176af6f7c9fe6
Facundo Agustín Fernández <facuassain@gmail.com>
2021-11-20 00:51:12 -0300

    home not displayed if there is no username key in localStorage
    If there is no username registered, the user is redirect to index.html

--- src/home.js
+++ src/home.js
@@ -1,7 +1,13 @@
-const username = localStorage.getItem('username')
-const usernameH2 = document.createElement("h2")
-usernameH2.classList.add("text-light")
-usernameH2.classList.add("main-text")
-usernameH2.appendChild(document.createTextNode(`Bienvenide ${username}`))
-const usernameDiv = document.getElementById("username")
-usernameDiv.appendChild(usernameH2)
+const load = () => {
+    const username = localStorage.getItem('username')
+    if(!username) window.location.href = "index.html"
+    const usernameH2 = document.createElement("h2")
+    usernameH2.classList.add("text-light")
+    usernameH2.classList.add("main-text")
+    usernameH2.appendChild(document.createTextNode(`Bienvenide ${username}`))
+    const usernameDiv = document.getElementById("username")
+    usernameDiv.appendChild(usernameH2)
+}
+
+document.addEventListener('DOMContentLoaded', load)
+
