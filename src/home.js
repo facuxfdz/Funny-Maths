56c9e8b3394f51ab359d7f0b4731af655e7112bb
Facundo Agustín Fernández <facuassain@gmail.com>
2021-11-23 22:20:37 -0300

    Jquery added
    getElementById and similar were changed to Jquery syntax
    
    If deleted in index.js

--- src/home.js
+++ src/home.js
@@ -1,13 +1,6 @@
-const load = () => {
+$(() => {
     const username = localStorage.getItem('username')
     if(!username) window.location.href = "index.html"
-    const usernameH2 = document.createElement("h2")
-    usernameH2.classList.add("text-light")
-    usernameH2.classList.add("main-text")
-    usernameH2.appendChild(document.createTextNode(`Bienvenide ${username}`))
-    const usernameDiv = document.getElementById("username")
-    usernameDiv.appendChild(usernameH2)
-}
-
-document.addEventListener('DOMContentLoaded', load)
-
+    const usernameDiv = $('#username')
+    usernameDiv.append(`<h2 class='text-light main-text'>Bienvenide ${username}</h2>`)
+})
--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 const saveName = () => {
-    const name = document.getElementById("name")
-    if(localStorage.getItem('username') !== name.value){
-        localStorage.setItem('username',name.value)
-        window.location.href = "home.html"    
-    }
+    const name = $('#name')[0]
+    localStorage.setItem('username',name.value)
+    window.location.href = "home.html"    
 }
 
-const botonGuardarNombre = document.getElementById("guardarNombre")
-botonGuardarNombre.addEventListener("click",saveName)    
-
+$('#guardarNombre').on("click",saveName)
